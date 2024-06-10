4a5bb5af388d1498fb7fd170d9fbcf0264ffed27
ZedFh <zakaria1fahim@gmail.com>
2024-06-10 17:42:46 +0200

    second commit

--- src/main/java/Rest/UserAPI.java
+++ src/main/java/Rest/UserAPI.java
@@ -2,21 +2,24 @@ package Rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import DAO.User;
+import Manager.ManageDAO;
 
 @Path("/user")
 public class UserAPI {
 
-    public UserAPI() {
-        // Constructor
-    }
+
 
     @GET
-    @Path("/get")
+    @Path("/get/{id}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String getUserById() {
-    	System.out.println("333");
-        return "test";
+    public String getUserById(@PathParam("id") Integer id) {
+    	ManageDAO<User> uM = new ManageDAO<User>(User.class);
+    	User u = (User) uM.getObjectById(id);
+        return u.toString();
     }
 }
